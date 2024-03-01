4c2e85df482b94d3cb722be04883d3b77f6688ad
Maxi Pelizzoni <maxi96p@gmail.com>
2024-02-29 23:02:56 -0300

    Icon app changed, added Splash screen

--- app/src/main/java/com/maxidev/pexis/MainActivity.kt
+++ app/src/main/java/com/maxidev/pexis/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.maxidev.pexis.ui.navigation.NavGraph
 import com.maxidev.pexis.ui.theme.PexisTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -14,9 +15,10 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
-            PexisTheme {
+            PexisTheme(darkTheme = false) {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
